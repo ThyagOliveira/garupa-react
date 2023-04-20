0c080df3252f979f79fd0f0b880520e8ddd25473
ThyagOliveira <taothyago@gmail.com>
2023-04-20 15:00:13 +0100

    :gem: Add pagination component

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,9 +4,8 @@ const server = axios.create({
   baseURL: 'https://api.punkapi.com/v2/',
 });
 
-const loadRandomBeers = () => server.get('beers/random/');
-const loadBeerPage = (page, per_page) =>
-  server.get('beers?page=' + page + '&per_page=' + per_page);
+const loadRandomBeers = () => server.get('beers/');
+const loadBeerPage = (page) => server.get('beers?page=' + page);
 
 const api = {
   loadRandomBeers,
