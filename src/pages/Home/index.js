1a1a170df8f8879049a2beec30742d1e0a3daebb
ThyagOliveira <taothyago@gmail.com>
2023-04-21 18:51:18 +0100

    :gem: :construction: Add login page and private route

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../../components/Card';
 import PaginationComponent from '../../components/Pagination';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import api from '../../utils/api';
+import auth from '../../utils/auth';
 import './styles.css';
 
 export default function HomePage() {
@@ -21,29 +23,47 @@ export default function HomePage() {
     setPage(page);
   };
 
+  function handleLogout() {
+    auth.clearLocalStorage();
+    window.location.reload();
+  }
+
   return (
-    <div className="homeContainer">
-      {beers.map((item) => (
-        <Card
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          image_url={item.image_url}
-          tagline={item.tagline}
-          first_brewed={item.first_brewed}
-          attenuation_level={item.attenuation_level}
-          ibu={item.ibu == null ? 0 : item.ibu}
-          abv={item.abv == null ? 0 : item.abv}
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Garupa
+          </Typography>
+          <Button color="inherit" onClick={handleLogout}>
+            Sair
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <div className="homeContainer">
+        {beers.map((item) => (
+          <Card
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image_url={item.image_url}
+            tagline={item.tagline}
+            first_brewed={item.first_brewed}
+            attenuation_level={item.attenuation_level}
+            ibu={item.ibu == null ? 0 : item.ibu}
+            abv={item.abv == null ? 0 : item.abv}
+          />
+        ))}
+        <PaginationComponent
+          spacing={2}
+          count={10}
+          color={'secondary'}
+          shape={'rounded'}
+          variant={'outlined'}
+          onChange={handleGetPage}
         />
-      ))}
-      <PaginationComponent
-        spacing={2}
-        count={10}
-        color={'secondary'}
-        shape={'rounded'}
-        variant={'outlined'}
-        onChange={handleGetPage}
-      />
-    </div>
+      </div>
+    </>
   );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 import HomePage from '../pages/Home';
+import LoginPage from '../pages/Login';
+import auth from '../utils/auth';
+
+const PrivateRoute = ({ children }) => {
+  const token = auth.getAuthToken();
+  let location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route
+        exact
+        path="/"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
